d8d017b65338b2211544a9b1626e89285051761e
andaleeb ali <s1885898@ed.ac.uk>
2023-06-28 23:53:14 +0100

    update to show exporting

--- segmentation/k-means2.py
+++ segmentation/k-means2.py
@@ -15,7 +15,7 @@ transposed_img = np.transpose(raster_data, (1, 2, 0))
 reshaped_data = transposed_img.reshape(-1, transposed_img.shape[-1])
 
 # Create the K-means clustering model
-model = KMeans(n_clusters=5, random_state=90)
+model = KMeans(n_clusters=4, random_state=10)
 
 # Fit the model to the reshaped data
 model.fit(reshaped_data)
@@ -26,35 +26,30 @@ labels = model.labels_
 # Reshape the labels array back to the original raster shape
 labels_reshaped = labels.reshape(transposed_img.shape[:-1])
 
-# Assign unique colors to each cluster label
-colors = np.array([
-    [0, 32, 46], 
-    #[0, 63, 92], 
-    [44, 72, 117], 
-    #[138, 80, 143], 
-    [188,80,144], 
-    #[255,99,97],
-    [255,133,49],
-    #[255,166,0], 
-    [255,211,128]])
+# Assign unique grayscale values to each cluster label
+grayscale_values = np.array([
+    0,  # Class 0
+    85,  # Class 1
+    170,  # Class 2
+    200,  # Class 3
+    255  # Class 4
+])
 
-# Create the classified image
-classified_image = colors[labels_reshaped]
+# Create the grayscale image based on the assigned grayscale values
+grayscale_image = grayscale_values[labels_reshaped]
 
-# Display the classified image
-plt.imshow(classified_image)
-plt.axis('off')
-plt.show()
-
-grayscale_image = np.dot(classified_image, [0.2989, 0.587, 0.114])
 # Get the metadata from the original raster dataset
 meta = opentif.meta
 
-# Update the metadata to match the classified image
+# Update the metadata to match the grayscale image
 meta.update(count=1, dtype=str(grayscale_image.dtype))
 
-# Save the classified image as a GeoTIFF
-output_file = 'classified_image.tif'
+# Save the grayscale image as a GeoTIFF
+output_file = 'grayscale_image_test.tif'
 with rasterio.open(output_file, 'w', **meta) as dst:
     dst.write(grayscale_image, 1)
 
+# Display the grayscale image
+plt.imshow(grayscale_image, cmap='gray')
+plt.axis('off')
+plt.show()
